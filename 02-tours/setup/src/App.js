be2a66b2fcc5a49888e09f9b4dbfaee5df4f80d2
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-17 15:48:29 -0800

    set up loading conditional

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,19 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+	const [loading, setLoading] = useState(true)
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		)
+	}
+	return (
+		<main>
+			<Tours />
+		</main>
+	)
 }
 
 export default App
