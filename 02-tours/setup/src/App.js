debb51e83cd4d575faab04ac82451d82beec1391
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-17 16:13:39 -0800

    fetch tours async and console.log them

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -6,6 +6,19 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 function App() {
 	const [loading, setLoading] = useState(true)
+	const [tours, setTours] = useState([])
+
+	const fetchTours = async () => {
+		setLoading(true)
+		const response = await fetch(url)
+		const tours = await response.json()
+		console.log(tours)
+	}
+
+	useEffect(() => {
+		fetchTours()
+	}, [])
+
 	if (loading) {
 		return (
 			<main>
